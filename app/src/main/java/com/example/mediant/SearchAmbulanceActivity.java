ad7e782fdf773ca788345abd8da4c7e1534896c9
Asad <bedar.24csedu.031@gmail.com>
2019-10-20 18:18:42 +0600

    searchview update for ambulance

--- app/src/main/java/com/example/mediant/SearchAmbulanceActivity.java
+++ app/src/main/java/com/example/mediant/SearchAmbulanceActivity.java
@@ -6,18 +6,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.SearchView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 public class SearchAmbulanceActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
-
+    Spinner spinner;
+    ArrayAdapter<CharSequence> adapter;
+    SearchView ambusearch;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_ambulance);
 
-        Spinner spinner = findViewById(R.id.spinner_seach_ambu);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.Searching_Ambulance, android.R.layout.simple_spinner_item);
+        spinner = findViewById(R.id.spinner_seach_ambu);
+        ambusearch = findViewById(R.id.id_search_ambu);
+        adapter = ArrayAdapter.createFromResource(this, R.array.Searching_Ambulance, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(this);
@@ -27,15 +31,17 @@ public class SearchAmbulanceActivity extends AppCompatActivity implements Adapte
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
         String item = adapterView.getItemAtPosition(i).toString();
         if(item.equals("City")){
-            
+            ambusearch.setQueryHint("Enter city");
+
         }
 
         else if(item.equals("Name")){
+            ambusearch.setQueryHint("Enter ambulance name");
 
         }
 
         else if(item.equals("Contact Number")){
-
+            ambusearch.setQueryHint("Enter contact number");
         }
     }
 
