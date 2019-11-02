727a0f8f57f545c2731ae251cbb373d6488c192c
Asad <bedar.24csedu.031@gmail.com>
2019-11-03 02:57:10 +0600

    showing details medicine info

---
+++ app/src/main/java/com/example/mediant/MedicineDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.example.mediant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MedicineDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_medicine_details);
+    }
+}
