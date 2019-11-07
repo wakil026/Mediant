b1e1027cac6d870bafd7c1b42d677a80dd9e3c38
Asad <bedar.24csedu.031@gmail.com>
2019-11-08 00:15:06 +0600

    added feature of phone call ambulance

--- app/src/main/java/com/example/mediant/AllAmbulanceActivity.java
+++ app/src/main/java/com/example/mediant/AllAmbulanceActivity.java
@@ -3,9 +3,15 @@ package com.example.mediant;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Toast;
 
@@ -25,6 +31,8 @@ public class AllAmbulanceActivity extends AppCompatActivity {
     RecyclerView.LayoutManager layoutManager;
     FirebaseFirestore firebaseFirestore;
     CustomAdapterforAllAmbulance adapter;
+    public final int REQUEST_CALL = 1;
+    public String phoneNumber;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -64,5 +72,30 @@ public class AllAmbulanceActivity extends AppCompatActivity {
 
                     }
                 });
+    }
+    public void makeCall(){
+        if (ContextCompat.checkSelfPermission(AllAmbulanceActivity.this,
+                Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(AllAmbulanceActivity.this,
+                    new String[]{Manifest.permission.CALL_PHONE}, REQUEST_CALL);
+        }
+        else {
+            String dial = "tel:" + phoneNumber;
+            startActivity(new Intent(Intent.ACTION_CALL, Uri.parse(dial)));
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_CALL) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                makeCall();
+            } else {
+                Toast.makeText(this, "Permission DENIED", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+    public void  getPhoneNumber(int index) {
+        phoneNumber = modelList.get(index).getContactNumber();
     }
 }
