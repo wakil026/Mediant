f1ad302c15458e0d99e37a4a60dedb99e88c39d3
Wakil <wakilurislam@gmail.com>
2019-11-17 16:59:00 +0600

    layout updated

--- app/src/main/java/com/example/mediant/HomeAdminActivity.java
+++ app/src/main/java/com/example/mediant/HomeAdminActivity.java
@@ -25,24 +25,8 @@ public class HomeAdminActivity extends AppCompatActivity {
         removeMedicine = (Button) findViewById(R.id.removemedicine_button);
         addAmbulance = (Button) findViewById(R.id.Addambulance_button);
         removeAmbulance = (Button) findViewById(R.id.removeambulance_button);
-        searchAmbulance = (Button) findViewById(R.id.button_search_ambu);
-        searchMedicine = (Button) findViewById(R.id.button_search_medicine);
         addAdmin = (Button)findViewById(R.id.addAdminButtonId);
         removeAdmin = (Button)findViewById(R.id.removeAdminButtonId);
-
-        searchMedicine.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                searchingMedicine();
-            }
-        });
-
-        searchAmbulance.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                searchingAmbulance();
-            }
-        });
 
 
         addMedicine.setOnClickListener(new View.OnClickListener() {
@@ -92,15 +76,6 @@ public class HomeAdminActivity extends AppCompatActivity {
 
     }
 
-    public void searchingMedicine(){
-        Intent intent = new Intent(this, SearchMedicineActivity.class);
-        startActivity(intent);
-    }
-
-    public void searchingAmbulance(){
-        Intent intent = new Intent(this, SearchAmbulanceActivity.class);
-        startActivity(intent);
-    }
 
     private void addingAdmin() {
         Intent intent = new Intent(getApplicationContext(),AddAdminActivity.class);
